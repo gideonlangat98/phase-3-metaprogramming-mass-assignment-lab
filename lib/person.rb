06daf88f66aabcef30b5114de12e1026833a30b7
gideonlangat98 <gideonlangat98@gmail.com>
2022-08-24 13:34:29 +0300

    commit tree 18ef5702c4861ba2d6e4c1032b0e66bb29398771

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
   # your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height
+  attr_accessor :weight, :handed, :complexion, :t_shirt_size
+  attr_accessor :wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
+
+spencer_attributes = { name: "Spencer", hair_color: "N/A"}
+spencer = Person.new(spencer_attributes)
+spencer.name
+spencer.hair_color
